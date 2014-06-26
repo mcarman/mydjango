051fbea8d916d068036bfef18c2f7501297f6910
mcarman <carman.md@live.com>
2014-06-26 03:21:14 -0600

    revert to state of project at set boilerplate

--- mydjango/apps/blogengine/admin.py
+++ mydjango/apps/blogengine/admin.py
@@ -1,17 +1,3 @@
-#blogengine.admin
-import models
 from django.contrib import admin
-from django.contrib.auth.models import User
 
-
-class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
-    exclude = ('author',)
-
-    def save_model(self, request, obj, form, change):
-        obj.author = request.user
-        obj.save()
-
-# admin.site.register(models.Category)
-# admin.site.register(models.Tag)
-admin.site.register(models.Post, PostAdmin)
+# Register your models here.
--- mydjango/urls.py
+++ mydjango/urls.py
@@ -1,19 +1,14 @@
-#mydjango.urls
+from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
+
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
 
+
+# See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_tutorial_blog_ng.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    # Admin panel and documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
-    # Blog URLs
-    url(r'', include('app.blogengine.urls')),
-
-    # Flat pages
-    url(r'', include('django.contrib.flatpages.urls')),
 )
